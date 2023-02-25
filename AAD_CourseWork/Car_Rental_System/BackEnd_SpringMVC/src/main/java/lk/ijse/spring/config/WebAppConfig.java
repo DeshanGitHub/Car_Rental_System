ee6a8759c0eb7b8afd24cce42a2cba8112fd7c74
Kavindu Deshan <kkavindu.deshan@gmail.com>
2023-02-25 05:45:01 +0530

    Add AppWideAdivsor

--- AAD_CourseWork/Car_Rental_System/BackEnd_SpringMVC/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ AAD_CourseWork/Car_Rental_System/BackEnd_SpringMVC/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -1,11 +1,13 @@
 package lk.ijse.spring.config;
 
+import lk.ijse.spring.advisor.AppWideAdvisor;
+import lk.ijse.spring.controller.EmployeeController;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@ComponentScan(basePackages = "lk.ijse.spring.controller")
+@ComponentScan(basePackageClasses = {EmployeeController.class, AppWideAdvisor.class})
 @EnableWebMvc
 public class WebAppConfig {
 }
